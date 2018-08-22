c9da1063c69241df898b244dd45931cf4fb29c1d
Alex Ung <alexung.ucsb@gmail.com>
2018-08-21 20:06:42 -0700

    d3 intro with .enter and scaleLinear xScale yScale

--- src/App.js
+++ src/App.js
@@ -1,11 +1,101 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as d3 from 'd3';
+
+import movies from './movies';
+
 import './App.css';
 
+// const quotes = [
+//   "Go ahead, make my day.",
+//   "I'll be back.",
+//   "May the Force be with you.",
+//   "There's no place like home.",
+//   "You're gonna need a bigger boat."
+// ];
+
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 600,
+      height: 600,
+      svg: null
+    }
+  }
+
+  componentDidMount = () => {
+    const { width, height } = this.state;
+    const svg = d3
+      .select('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    this.plotMovies();
+    this.setState({ svg })
+  }
+
+  plotMovies = () => {
+    const { width, height } = this.state;
+
+    // const scale = d3.scaleLinear().domain([1,5]).range([12, 70]);
+    // ^^ this maps values between 1 - 5 to 12 - 70
+
+    // d3 has method to calculate extreme values of data
+    // d3.extent(movies, movie => movie.runtime) ==> returns an array [smallestNum, largestNum]
+
+    const runtimeDomain = d3.extent(movies, movie => movie.runtime);
+    const xScale = d3.scaleLinear().domain(runtimeDomain).range([0, width]);
+
+    // IMPORTANT
+    // height is in first coord bc svg's read y coords from 0 being at top and max being at bottom
+    const yScale = d3.scaleLinear().domain([0,1]).range([height, 0]);
+
+    d3.select('svg')
+      .selectAll('circle')
+      .data(movies)
+      .enter()
+      // take nodes out of .enter() purgatory and append them to page as circles
+      .append('circle')
+      .attr('cx', movieObj => xScale(movieObj.runtime))
+      .attr('cy', movieObj => yScale(movieObj.freshness))
+      .attr('r', 8)
+      .attr('fill', 'blue');
+  }
+
+  // componentDidMount() {
+  //   // IMPORTANT
+  //   // Rather than accessing nodes via _groups[0][0], -- this is dev way of saying this is bad
+  //   // use d3.select('h1').node()
+  //
+  //   // lisp functional programming
+  //
+  //   d3
+  //     // we're making sure that what happens in the id container
+  //     .select('#container')
+  //     /* looking for p tags on the page but there are NONE.
+  //      Gives a Selection Object (which should be empty) back, not 'null'
+  //      Here is where we can insert each quote */
+  //     .selectAll('p')
+  //     /* d3 creates as many 'p' _enter nodes as there are quotes
+  //      if there's leftover elements that are not being used by d3,
+  //      it puts them in the _exit selection */
+  //     .data(quotes)
+  //     /* .enter gives us new nodes within _enter.
+  //      .exit gives us nodes are being unused
+  //      _groups gives the update()
+  //      https://bost.ocks.org/mike/join/ */
+  //     .enter()
+  //     /* for each enter node, it will append a p tag with the text of that data inside */
+  //     .append('p')
+  //     .text(d => d);
+  // }
+
   render() {
     return (
-      <div>
+      <div id='container'>
+        <svg></svg>
       </div>
     );
   }
---
+++ src/movies.js
@@ -0,0 +1,203 @@
+const movies = [{
+  title: "Wonder Woman",
+  release: new Date("June 2 2017"),
+  rating: "PG-13",
+  runtime: 141,
+  total: 412563408,
+  openingTotal: 103251471,
+  maxTheaters: 4165,
+  daysOpen: 161,
+  freshness: 0.92
+}, {
+  title: "Guardians of the Galaxy Vol. 2",
+  release: new Date("May 5 2017"),
+  rating: "PG-13",
+  runtime: 137,
+  total: 389813101,
+  openingTotal: 146510104,
+  maxTheaters: 4347,
+  daysOpen: 140,
+  freshness: 0.83
+}, {
+  title: "Spider-Man: Homecoming",
+  release: new Date("July 7 2017"),
+  rating: "PG-13",
+  runtime: 133,
+  total: 334201140,
+  openingTotal: 117027503,
+  maxTheaters: 4348,
+  daysOpen: 147,
+  freshness: 0.92
+}, {
+  title: "Despicable Me 3",
+  release: new Date("June 30 2017"),
+  rating: "PG",
+  runtime: 90,
+  total: 264624300,
+  openingTotal: 72434025,
+  maxTheaters: 4535,
+  daysOpen: 175,
+  freshness: 0.60
+}, {
+  title: "Dunkirk",
+  release: new Date("July 21 2017"),
+  rating: "PG-13",
+  runtime: 106,
+  total: 188045546,
+  openingTotal: 50513488,
+  maxTheaters: 4014,
+  daysOpen: 126,
+  freshness: 0.92
+}, {
+  title: "Pirates of the Caribbean: Dead Men Tell No Tales",
+  release: new Date("May 26 2017"),
+  rating: "PG-13",
+  runtime: 129,
+  total: 172558876,
+  openingTotal: 62983253,
+  maxTheaters: 4276,
+  daysOpen: 119,
+  freshness: 0.30
+}, {
+  title: "Cars 3",
+  release: new Date("June 16 2017"),
+  rating: "G",
+  runtime: 102,
+  total: 152901115,
+  openingTotal: 53688680,
+  maxTheaters: 4256,
+  daysOpen: 140,
+  freshness: 0.68
+}, {
+  title: "War for the Planet of the Apes",
+  release: new Date("July 14 2017"),
+  rating: "PG-13",
+  runtime: 140,
+  total: 146880162,
+  openingTotal: 56262929,
+  maxTheaters: 4100,
+  daysOpen: 140,
+  freshness: 0.93
+}, {
+  title: "Transformers: The Last Knight",
+  release: new Date("June 21 2017"),
+  rating: "PG-13",
+  runtime: 149,
+  total: 130168683,
+  openingTotal: 44680073,
+  maxTheaters: 4132,
+  daysOpen: 65,
+  freshness: 0.16
+}, {
+  title: "Girls Trip",
+  release: new Date("July 21 2017"),
+  rating: "R",
+  runtime: 122,
+  total: 115171585,
+  openingTotal: 31201920,
+  maxTheaters: 2648,
+  daysOpen: 91,
+  freshness: 0.90
+}, {
+  title: "Baby Driver",
+  release: new Date("June 28 2017"),
+  rating: "R",
+  runtime: 112,
+  total: 107825862,
+  openingTotal: 20553320,
+  maxTheaters: 3226,
+  daysOpen: 114,
+  freshness: 0.93
+}, {
+  title: "Annabelle: Creation",
+  release: new Date("August 11 2017"),
+  rating: "R",
+  runtime: 109,
+  total: 102092201,
+  openingTotal: 35006404,
+  maxTheaters: 3565,
+  daysOpen: 84,
+  freshness: 0.69
+}, {
+  title: "The Emoji Movie",
+  release: new Date("July 28 2017"),
+  rating: "PG",
+  runtime: 96,
+  total: 86089513,
+  openingTotal: 24531923,
+  maxTheaters: 4075,
+  daysOpen: 126,
+  freshness: 0.09
+}, {
+  title: "The Mummy (2017)",
+  release: new Date("June 9 2017"),
+  rating: "PG-13",
+  runtime: 107,
+  total: 80227895,
+  openingTotal: 31688375,
+  maxTheaters: 4035,
+  daysOpen: 77,
+  freshness: 0.16
+}, {
+  title: "The Hitman's Bodyguard",
+  release: new Date("August 18 2017"),
+  rating: "R",
+  runtime: 118,
+  total: 75468583,
+  openingTotal: 21384504,
+  maxTheaters: 3377,
+  daysOpen: 77,
+  freshness: 0.40
+}, {
+  title: "Alien: Covenant",
+  release: new Date("May 19 2017"),
+  rating: "R",
+  runtime: 122,
+  total: 74262031,
+  openingTotal: 36160621,
+  maxTheaters: 3772,
+  daysOpen: 105,
+  freshness: 0.68
+}, {
+  title: "Captain Underpants: The First Epic Movie",
+  release: new Date("June 2 2017"),
+  rating: "PG",
+  runtime: 89,
+  total: 73921000,
+  openingTotal: 23851539,
+  maxTheaters: 3529,
+  daysOpen: 161,
+  freshness: 0.86
+}, {
+  title: "Baywatch",
+  release: new Date("May 25 2017"),
+  rating: "R",
+  runtime: 116,
+  total: 58060186,
+  openingTotal: 18503871,
+  maxTheaters: 3647,
+  daysOpen: 64,
+  freshness: 0.18
+}, {
+  title: "Atomic Blonde",
+  release: new Date("July 28 2017"),
+  rating: "R",
+  runtime: 115,
+  total: 51687870,
+  openingTotal: 18286420,
+  maxTheaters: 3326,
+  daysOpen: 70,
+  freshness: 0.77
+}, {
+  title: "The Dark Tower",
+  release: new Date("August 4 2017"),
+  rating: "PG-13",
+  runtime: 95,
+  total: 50701325,
+  openingTotal: 19153698,
+  maxTheaters: 3451,
+  daysOpen: 77,
+  freshness: 0.16
+}];
+
+export default movies;
