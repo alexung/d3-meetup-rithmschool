7970afb112a55ff3f1d12b4f509f509faa6d218c
Alex Ung <alexung.ucsb@gmail.com>
2018-08-22 15:06:50 -0700

    d3 adding in axes and hover on data

--- src/App.js
+++ src/App.js
@@ -21,7 +21,13 @@ class App extends Component {
     this.state = {
       width: 600,
       height: 600,
-      svg: null
+      svg: null,
+      padding: {
+        top: 40,
+        right: 20,
+        bottom: 20,
+        left: 70
+      }
     }
   }
 
@@ -31,13 +37,21 @@ class App extends Component {
       .select('svg')
       .attr('width', width)
       .attr('height', height);
+    const tooltip = d3
+      .select('body')
+      .append('div')
+      .attr('class', 'tooltip')
+      .style('opacity', 0)
+      .style('position', 'absolute')
+      .style('background-color', '#000')
+      .style('color', '#FFF')
+      .style('padding', '3px');
 
-    this.plotMovies();
-    this.setState({ svg })
+    this.setState({ svg, tooltip }, () => this.plotMovies());
   }
 
   plotMovies = () => {
-    const { width, height } = this.state;
+    const { width, height, padding, tooltip } = this.state;
 
     // const scale = d3.scaleLinear().domain([1,5]).range([12, 70]);
     // ^^ this maps values between 1 - 5 to 12 - 70
@@ -46,11 +60,19 @@ class App extends Component {
     // d3.extent(movies, movie => movie.runtime) ==> returns an array [smallestNum, largestNum]
 
     const runtimeDomain = d3.extent(movies, movie => movie.runtime);
-    const xScale = d3.scaleLinear().domain(runtimeDomain).range([0, width]);
+    const xScale = d3.scaleLinear().domain(runtimeDomain).range([padding.left, width - padding.right]);
 
     // IMPORTANT
     // height is in first coord bc svg's read y coords from 0 being at top and max being at bottom
-    const yScale = d3.scaleLinear().domain([0,1]).range([height, 0]);
+    const yScale = d3.scaleLinear().domain([0,1]).range([height - padding.top, padding.bottom]);
+
+    const [ rMin, rMax ] = d3.extent(movies, movie => movie.maxTheaters);
+    const rScale = d3.scaleLinear().domain([rMin, rMax]).range([5, 20]);
+
+    const colorScale = d3
+      .scaleLinear()
+      .domain([0, 1])
+      .range(['green', 'red']);
 
     d3.select('svg')
       .selectAll('circle')
@@ -60,8 +82,57 @@ class App extends Component {
       .append('circle')
       .attr('cx', movieObj => xScale(movieObj.runtime))
       .attr('cy', movieObj => yScale(movieObj.freshness))
-      .attr('r', 8)
-      .attr('fill', 'blue');
+      .attr('r', d => rScale(d.maxTheaters))
+      .attr('fill', d => colorScale(d.freshness))
+      .attr('stroke', 'black')
+      .on('mouseover', d => {
+        tooltip
+          .text(d.title)
+          .style('opacity', 1)
+          .style('left', `${d3.event.pageX}px`)
+          .style('top', `${d3.event.pageY}px`);
+      })
+      .on('mouseout', d => tooltip.style('opacity', 0));
+
+    this.setState({ xScale, yScale }, () => this.plotAxes());
+  }
+
+  plotAxes = () => {
+    this.xAxis();
+    this.yAxis();
+  }
+
+  xAxis = () => {
+    const { svg, width, height, padding, xScale } = this.state;
+    svg
+      .append('g')
+      .attr('class', 'x-axis')
+      .attr('transform', `translate(0,${(height - padding.top)})`)
+      .call(d3.axisBottom(xScale));
+
+    // x-axis label
+    svg
+      .append('text')
+      .attr('transform',
+            `translate(${(width/2)}, ${height})`)
+      .style('text-anchor', 'middle')
+      .text('Runtime');
+  }
+
+  yAxis = () => {
+    const { svg, padding, yScale, height, width } = this.state;
+    svg
+      .append('g')
+      .attr("class", "y-axis")
+      .attr('transform', `translate(${padding.left}, 0)`)
+      .call(d3.axisLeft(yScale));
+    // y-axis label
+    svg
+      .append('text')
+      .attr('transform',
+            `translate(${20}, ${height/2}) rotate(-90)`)
+      .style('text-anchor', 'middle')
+      .text('RottenTomatoes Score');
   }
 
   // componentDidMount() {
